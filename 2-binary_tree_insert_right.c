81865d77aa14ad4658ba8745dd48d3149ccebcb2
ELCBAh <6211@holbertonstudents.com>
2023-05-15 15:45:49 -0300

    binary tree insert left fixed segmentation fault

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -29,10 +29,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node->left = NULL;
 	new_node->parent = parent;
 
-	if (parent->left != NULL)
-		parent->left->parent = new_node;
+	if (parent->right != NULL)
+		parent->right->parent = new_node;
 
-	parent->left = new_node;
+	parent->right = new_node;
 
 	return (new_node);
 }
