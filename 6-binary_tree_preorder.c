0720aeaa99a9c5a3e3944f86a7ee5f7a87166543
ELCBAh <6211@holbertonstudents.com>
2023-05-15 21:29:49 -0300

    fixed extra spaces

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -4,7 +4,7 @@
  * binary_tree_preorder - initialize prototype
  *
  * @tree: pointer to the root node of the tree to delete
- * 
+ *
  * @func: pointer to call the function
  *
  * Description: traverses a binary tree using pre-order traversal
@@ -15,7 +15,8 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree != NULL)
 	{
 		if (func == NULL || tree == NULL)
-			return;	
+			return;
+
 		func(tree->n);
 		binary_tree_preorder(tree->left, func);
 		binary_tree_preorder(tree->right, func);
