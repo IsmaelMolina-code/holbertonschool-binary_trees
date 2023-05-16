e61d6f1fd4be3a7bd1854ec491b024d2d63458b7
ELCBAh <6211@holbertonstudents.com>
2023-05-15 22:23:36 -0300

    making small changes to the code and trying to fix output not updating, return value still being 0

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,7 +14,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	heightleft += binary_tree_height(tree->left);
-	heightright += binary_tree_height(tree->right);
-	return (heightleft > heightright ? heightleft : heightright);
+	heightleft = binary_tree_height(tree->left);
+	heightright = binary_tree_height(tree->right);
+	return (heightleft > heightright ? heightleft + 1 : heightright + 1);
 }
