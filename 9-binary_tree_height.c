a2fcbba35f816fc3271eee2fd8f011f8d4d089c2
ELCBAh <6211@holbertonstudents.com>
2023-05-15 22:17:49 -0300

    fixed height not counting properly and fixed betty coding style errors

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,27 +1,24 @@
 #include "binary_trees.h"
 /**
  * binary_tree_height - initializes prototype
- * 
+ *
  * @tree: pointer to node to measure height
- * 
+ *
  * Description: measures the height of a binary tree
- * 
+ *
  * Return: height of tree, otherwise 0
 */
-
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t heightleft = 0, heightright = 0;
+	size_t heightleft = 0, heightright = 0;
 
-        if (tree == NULL)
-                return (0);
-
-        heightleft = binary_tree_height(tree->left);
-        heightright = binary_tree_height(tree->right);
-
-        if (heightleft > heightright)
-                return (heightleft + 1);
-        else if (heightright >= heightleft)
-                return (heightright + 1);
+	if (tree == NULL)
+		return (0);
+	heightleft += binary_tree_height(tree->left);
+	heightright += binary_tree_height(tree->right);
+	if (heightleft > heightright)
+		return (heightleft);
+	else if (heightright >= heightleft)
+		return (heightright);
 	return (0);
 }
