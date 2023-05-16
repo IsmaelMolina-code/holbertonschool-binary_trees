da8ab3628159ad458d8155890ac2268571ee4178
ELCBAh <6211@holbertonstudents.com>
2023-05-15 21:33:53 -0300

    trailing whitespace

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - initialize prototype
+ * binary_tree_postorder - initialize prototype
  *
  * @tree: pointer to the root node of the tree to delete
  *
@@ -10,14 +10,15 @@
  * Description: traverses a binary tree using pre-order traversal
  */
 
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree != NULL)
 	{
 		if (func == NULL || tree == NULL)
 			return;
 
-		binary_tree_preorder(tree->left, func);
-		binary_tree_preorder(tree->right, func);
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postrder(tree->right, func);
+		func(tree->n);
 	}
 }
